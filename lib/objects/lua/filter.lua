d398da0797c88c4a48fcd0a63ab6d8ec8b430625
Moritz Heusinger <moritz.heusinger@gmail.com>
2020-05-25 09:58:32 +0200

    catch/handle json parse errors in lua scripts

--- lib/objects/lua/filter.lua
+++ lib/objects/lua/filter.lua
@@ -2,24 +2,20 @@
 local rep = {}
 -- local keys=redis.call("keys", KEYS[1].."*")
 local cursor = KEYS[5];
-local result=redis.call("SCAN", cursor, "MATCH", KEYS[1].."*", "COUNT", 500)
+local result = redis.call("SCAN", cursor, "MATCH", KEYS[1] .. "*", "COUNT", 500)
 cursor = result[1]
 local keys = result[2]
-local argStart=KEYS[1]..KEYS[2]
-local argEnd=KEYS[1]..KEYS[3]
-local type=KEYS[4]
-local obj
-local decoded
+local argStart = KEYS[1] .. KEYS[2]
+local argEnd = KEYS[1] .. KEYS[3]
+local type = KEYS[4]
 -- function(doc) { if (doc.type === "chart") emit(doc._id, doc); }
-for _,key in ipairs(keys) do
+for _, key in ipairs(keys) do
     if (key >= argStart and key < argEnd) then
-        obj = redis.call("get", key)
-        if (obj ~= nil and obj ~= "") then
-            decoded = cjson.decode(obj)
-            if (decoded and decoded.type == type) then
-                rep[#rep+1] = obj
-            end
+        local obj = redis.call("get", key)
+        local success, decoded = pcall(cjson.decode, obj)
+        if (success and decoded.type == type) then
+            rep[#rep + 1] = obj
         end
     end
 end
-return {rep, cursor}
+return { rep, cursor }
--- lib/objects/lua/script.lua
+++ lib/objects/lua/script.lua
@@ -1,22 +1,21 @@
 -- design: script
 -- search: javascript
 local rep = {}
--- local keys=redis.call("keys", KEYS[1].."*")
 local cursor = KEYS[4];
-local result=redis.call("SCAN", cursor, "MATCH", KEYS[1].."*", "COUNT", 500)
+local result = redis.call("SCAN", cursor, "MATCH", KEYS[1] .. "*", "COUNT", 500)
 cursor = result[1]
 local keys = result[2]
-local argStart=KEYS[1]..KEYS[2]
-local argEnd=KEYS[1]..KEYS[3]
-local obj
+local argStart = KEYS[1] .. KEYS[2]
+local argEnd = KEYS[1] .. KEYS[3]
 --  function(doc) {
 --      if (doc.type === "script" && doc.common.engineType.match(/^[jJ]ava[sS]cript|^[cC]offee[sS]cript|^[tT]ype[sS]cript|^Blockly/)) emit(doc.common.name, doc); }
 for _, key in ipairs(keys) do
-	if (key >= argStart and key < argEnd) then
-	    obj = redis.call("get", key)
-		if (obj ~= nil and obj ~= "" and cjson.decode(obj).type == "script") then
-            rep[#rep+1] = obj
+    if (key >= argStart and key < argEnd) then
+        local obj = redis.call("get", key)
+        local success, decoded = pcall(cjson.decode, obj)
+        if (success and decoded.type == "script") then
+            rep[#rep + 1] = obj
         end
-	end
+    end
 end
-return {rep, cursor}
+return { rep, cursor }
