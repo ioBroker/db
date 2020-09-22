a116f4cb55ba7c14ba5c018708c6a14cd0a6e9a0
Dominic Griesel <d.griesel@gmx.net>
2020-09-22 23:11:00 +0200

    incorporate review feedback, simplify logic

--- packages/db-base/lib/tools.js
+++ packages/db-base/lib/tools.js
@@ -3,14 +3,13 @@ const fs = require('fs');
 
 function getControllerDir() {
     const possibilities = ['iobroker.js-controller', 'ioBroker.js-controller'];
-    /** @type {string | null} */
-    let controllerPath = null;
     for (const pkg of possibilities) {
         try {
-            const possiblePath = require.resolve(pkg);
+            // package.json is guaranteed to be in the module root folder
+            // so once that is resolved, take the dirname and we're done
+            const possiblePath = require.resolve(`${pkg}/package.json`);
             if (fs.existsSync(possiblePath)) {
-                controllerPath = possiblePath;
-                break;
+                return path.dirname(possiblePath);
             }
         } catch (_a) {
             /* not found */
@@ -19,40 +18,29 @@ function getControllerDir() {
 
     // Apparently, checking vs null/undefined may miss the odd case of controllerPath being ""
     // Thus we check for falsyness, which includes failing on an empty path
-    if (!controllerPath) {
-        let checkPath = path.join(__dirname, '../..');
-        // Also check in the current check dir (along with iobroker.js-controller subdirs)
-        possibilities.unshift('');
-        outer: while (true) {
-            for (const pkg of possibilities) {
-                try {
-                    const possiblePath = path.join(checkPath, pkg);
-                    if (fs.existsSync(path.join(possiblePath, 'lib/tools.js'))) {
-                        controllerPath = possiblePath;
-                        break outer;
-                    }
-                } catch (_a) {
-                    // not found, continue with next possiblity
+    let checkPath = path.join(__dirname, '../..');
+    // Also check in the current check dir (along with iobroker.js-controller subdirs)
+    possibilities.unshift('');
+    while (true) {
+        for (const pkg of possibilities) {
+            try {
+                const possiblePath = path.join(checkPath, pkg);
+                if (fs.existsSync(path.join(possiblePath, 'lib/tools.js'))) {
+                    return possiblePath;
                 }
+            } catch (_a) {
+                // not found, continue with next possiblity
             }
+        }
 
-            // Controller not found here - go to the parent dir
-            const newPath = path.dirname(checkPath);
-            if (newPath === checkPath) {
-                // We already reached the root dir, abort
-                break;
-            }
-            checkPath = newPath;
+        // Controller not found here - go to the parent dir
+        const newPath = path.dirname(checkPath);
+        if (newPath === checkPath) {
+            // We already reached the root dir, abort
+            break;
         }
-        // ??? What is this supposed to do ???
-        if (controllerPath && !fs.existsSync(controllerPath)) {
-            controllerPath = null;
-        }
-    } else {
-        // ??? What is this supposed to do ???
-        controllerPath = path.dirname(controllerPath);
+        checkPath = newPath;
     }
-    return controllerPath;
 }
 
 module.exports = require(path.join(getControllerDir() || __dirname, 'lib/tools.js'));
